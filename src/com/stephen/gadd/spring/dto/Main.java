d7d53d7f1eee2539f9682c3f82e49c02e4e55d8e
Stephen Gadd <sgadd1805@gmail.com>
2016-02-13 16:43:11 -0500

    my first commit

---
+++ src/com/stephen/gadd/spring/dto/Main.java
@@ -0,0 +1,12 @@
+package com.stephen.gadd.spring.dto;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/stephen/gadd/spring/dto/Square.java
@@ -0,0 +1,9 @@
+package com.stephen.gadd.spring.dto;
+
+public class Square {
+
+	
+	public void test(){
+		System.out.println("test spring");
+	}
+}
